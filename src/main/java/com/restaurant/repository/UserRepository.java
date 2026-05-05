4088f2e8bb7f29dc575856f2687c7e2776600d77
youssif Mohamed fathy <youssifmohamed0990@email.com>
2026-05-06 00:51:30 +0300

    my commit

--- src/main/java/com/restaurant/repository/UserRepository.java
+++ src/main/java/com/restaurant/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.restaurant.repository;
 
-import org.springframework.stereotype.Repository;
+import com.restaurant.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-@Repository
-public interface UserRepository {
-    // TODO: Extend JpaRepository and add custom query methods
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
