51eea63a7e8ac75bd3fd8719a8739873faf07690
Seif-Eldin8 <S.Naga64599@student.aast.edu>
2026-05-07 17:20:14 +0300

    Repository

--- src/main/java/com/restaurant/repository/MenuItemRepository.java
+++ src/main/java/com/restaurant/repository/MenuItemRepository.java
@@ -1,8 +1,14 @@
 package com.restaurant.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.restaurant.model.MenuItem;
 
 @Repository
-public interface MenuItemRepository {
-    // TODO: Extend JpaRepository and add custom query methods
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+    List<MenuItem> findByAvailableTrue();
+    List<MenuItem> findByCategoryAndAvailableTrue(String category);
 }
--- src/main/java/com/restaurant/repository/OrderRepository.java
+++ src/main/java/com/restaurant/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.restaurant.repository;
 
-import org.springframework.stereotype.Repository;
+import com.restaurant.model.Order;
+import com.restaurant.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-@Repository
-public interface OrderRepository {
-    // TODO: Extend JpaRepository and add custom query methods
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserOrderByCreatedAtDesc(User user);
+    List<Order> findAllByOrderByCreatedAtDesc();
 }
